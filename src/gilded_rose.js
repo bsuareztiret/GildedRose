1b73da47e907fc779b68137710ea00d59bd45d45
bsuarez- <beny.suarez_piedra@me.com>
2022-08-26 20:11:17 +0200

    [WRITE]: Some clean up

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,41 +11,36 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    const debugFunction = (item) => {
-      console.log('SellIn:', item.sellIn);
-      console.log('Quality:', item.quality);
-    }
     const checkItemsName = (name) => {
       if (name.toUpperCase().includes('CONJURED')) {
-        return 'Conjured';
+        return 'CONJURED';
       } if (name === 'Aged Brie') {
-        return 'Cheese';
+        return 'CHEESE';
       } if (name === 'Backstage passes to a TAFKAL80ETC concert') {
-        return 'Ticket';
+        return 'TICKET';
       } if (name === 'Sulfuras, Hand of Ragnaros') {
-        return 'Legend';
+        return 'LEGEND';
       } else {
         return 'Standard';
       }
     }
+
     const checkItemsSellIn = (sellIn, type) => {
-      if (type === 'Ticket' && sellIn <= 5) {
-        return 'Third';
-      } else if (type === 'Ticket' && sellIn <= 10) {
-        return 'Twice';
+      if (type === 'TICKET' && sellIn <= 5) {
+        return 'third';
+      } else if (type === 'TICKET' && sellIn <= 10) {
+        return 'twice';
       } else if (sellIn > 0) {
-        return 'Positive';
+        return 'positive';
       } else if (sellIn === 0) {
-        return 'Zero'
+        return 'zero'
       } else if (sellIn < 0) {
-        return 'Negative';
+        return 'negative';
       }
     }
 
-    const checkItemsQuality = (qualityResult, type) => {
-      if (type === 'UP' && qualityResult <= 50) {
-        return true;
-      } else if (type === 'DOWN' && qualityResult >= 0) {
+    const checkItemsQuality = (qualityResult) => {
+      if (qualityResult <= 50 && qualityResult >= 0) {
         return true;
       } else {
         return false;
@@ -55,57 +50,44 @@ class Shop {
     for (var i = 0; i < this.items.length; i++) {
       const actualQuality = this.items[i].quality;
       const actualSellIn = this.items[i].sellIn;
-      // console.log('__________')
       const useCaseName = checkItemsName(this.items[i].name);
       const useCaseSellIn = checkItemsSellIn(this.items[i].sellIn, useCaseName);
-      if (useCaseName === 'Conjured') {
-        // console.log('useCaseName:', useCaseName);
-        // console.log('useCaseSellIn:', useCaseSellIn);
-        // debugFunction(this.items[i]);
+      if (useCaseName === 'CONJURED') {
         let conjuredQuality = 0;
-        if (useCaseSellIn === 'Positive' || useCaseSellIn === 'Zero') {
-          conjuredQuality = checkItemsQuality(actualQuality - 2, 'DOWN') === true ? actualQuality - 2 : actualQuality;
+        if (useCaseSellIn === 'positive' || useCaseSellIn === 'zero') {
+          conjuredQuality = checkItemsQuality(actualQuality - 2) === true ? actualQuality - 2 : actualQuality;
         } else {
-          conjuredQuality = checkItemsQuality(actualQuality - actualQuality, 'DOWN') === true ? (actualQuality - actualQuality) * 2 : actualQuality;
+          conjuredQuality = checkItemsQuality(actualQuality - actualQuality) === true ? (actualQuality - actualQuality) * 2 : actualQuality;
         }
         this.items[i].quality = conjuredQuality;
-        this.items[i].sellIn = actualSellIn - 1;
-      } else if (useCaseName === 'Cheese') {
-        // console.log('useCaseName:', useCaseName);
-        // console.log('useCaseSellIn:', useCaseSellIn);
-        // debugFunction(this.items[i]);
-        let cheeseQuality = checkItemsQuality(actualQuality + 1, 'UP') === true ? actualQuality + 1 : actualQuality;
+      } else if (useCaseName === 'CHEESE') {
+        let cheeseQuality = checkItemsQuality(actualQuality + 1) === true ? actualQuality + 1 : actualQuality;
         this.items[i].quality = cheeseQuality;
-        this.items[i].sellIn = actualSellIn - 1;
-      } else if (useCaseName === 'Ticket') {
-        // console.log('useCaseName:', useCaseName);
-        // console.log('useCaseSellIn:', useCaseSellIn);
-        // debugFunction(this.items[i]);
-        let ticketQuality = checkItemsQuality(actualQuality + 1, 'UP') === true ? actualQuality + 1 : actualQuality;
-        if (useCaseSellIn === 'Twice') {
-          ticketQuality = checkItemsQuality(actualQuality + 2, 'UP') === true ? actualQuality + 2 : ticketQuality;
-        } else if (useCaseSellIn === 'Third') {
-          ticketQuality = checkItemsQuality(actualQuality + 3, 'UP') === true ? actualQuality + 3 : ticketQuality;
+      } else if (useCaseName === 'TICKET') {
+        let ticketQuality = checkItemsQuality(actualQuality + 1) === true ? actualQuality + 1 : actualQuality;
+        if (useCaseSellIn === 'negative') {
+          ticketQuality = 0;
         }
-        // console.log('TICKETQUALITY', ticketQuality);
+        else if (useCaseSellIn === 'twice') {
+          ticketQuality = checkItemsQuality(actualQuality + 2) === true ? actualQuality + 2 : ticketQuality;
+        } else if (useCaseSellIn === 'third') {
+          ticketQuality = checkItemsQuality(actualQuality + 3) === true ? actualQuality + 3 : ticketQuality;
+        }
         this.items[i].quality = ticketQuality;
-        this.items[i].sellIn = actualSellIn - 1;
       } else if (useCaseName === 'Standard') {
-        // console.log('useCaseName:', useCaseName);
-        // console.log('useCaseSellIn:', useCaseSellIn);
-        // debugFunction(this.items[i]);
         let standarQuality = 0;
         this.items[i].sellIn = this.items[i].sellIn - 1;
-        if (useCaseSellIn === 'Positive' || useCaseSellIn === 'Zero') {
-          standarQuality = checkItemsQuality(actualQuality - 1, 'DOWN') === true ? actualQuality - 1 : actualQuality;
+        if (useCaseSellIn === 'positive' || useCaseSellIn === 'zero') {
+          standarQuality = checkItemsQuality(actualQuality - 1) === true ? actualQuality - 1 : actualQuality;
         } else {
-          standarQuality = checkItemsQuality(actualQuality - actualQuality, 'DOWN') === true ? actualQuality - 1 : actualQuality;
+          standarQuality = checkItemsQuality(actualQuality - actualQuality) === true ? actualQuality - 1 : actualQuality;
         }
         this.items[i].quality = standarQuality;
+      }
+      if (useCaseName !== 'LEGEND') {
         this.items[i].sellIn = actualSellIn - 1;
       }
     }
-
     return this.items;
   }
 }
