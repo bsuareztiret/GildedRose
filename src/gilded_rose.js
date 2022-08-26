9390107b10d42671a9508644b2049289a1b1eb61
bsuarez- <beny.suarez_piedra@me.com>
2022-08-26 15:50:13 +0200

    [WRITE]: I need a checkItemsQuality too

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,7 +12,7 @@ class Shop {
   }
   updateQuality() {
     // First of all, find the exceptions
-    const checkItems = (name) => {
+    const checkItemsName = (name) => {
       if (name.toUpperCase().includes('CONJURED')) {
         return 'Conjured';
       } if (name === 'Aged Brie') {
@@ -26,7 +26,28 @@ class Shop {
       }
     }
     // Maybe with just a function for all the update it will be good
-    const updateByTime = (sellIn, quality) => {
+    const checkItemsSellIn = (sellIn, type) => {
+      if (sellIn > 0) {
+        return 'Positive';
+      } else if (sellIn === 0) {
+        return 'Zero'
+      } else if (sellIn < 0) {
+        return 'Negative';
+      } else if (type === 'Ticket' && sellIn <= 5) {
+        return 'Third';
+      } else if (type === 'Ticket' && sellIn <= 10) {
+        return 'Twice';
+      }
+    }
+
+    const checkItemsQuality = (qualityResult, type) => {
+      if (type === 'UP' && quality < 50) {
+        return true;
+      } else if (type === 'DOWN' && quality >= 0) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
     const debugFunction = (item) => {
@@ -36,25 +57,50 @@ class Shop {
 
     // I can use some value to update them and pass it in the updatedByTime function
     for (var i = 0; i < this.items.length; i++) {
-      let sellInToAdd = 0;
-      let qualityToAdd = 0;
-      console.log('__________')
-      const useCase = checkItems(this.items[i].name);
-      if (useCase === 'Conjured') {
-        console.log('useCase:', useCase);
+      const actualQuality = this.items[i].quality;
+      const actualSellIn = this.items[i].sellIn;
+      // console.log('__________')
+      const useCaseName = checkItemsName(this.items[i].name);
+      const useCaseSellIn = checkItemsSellIn(this.items[i].sellIn);
+      if (useCaseName === 'Conjured') {
+        // console.log('useCaseName:', useCaseName);
+        // debugFunction(this.items[i]);
+
+        // quality down twice
+      } else if (useCaseName === 'Cheeese') {
+        console.log('useCaseName:', useCaseName);
         debugFunction(this.items[i]);
-      } else if (useCase === 'Cheeese') {
-        console.log('useCase:', useCase);
+
+        // quality up
+      } else if (useCaseName === 'Ticket') {
+        console.log('useCaseName:', useCaseName);
         debugFunction(this.items[i]);
-      } else if (useCase === 'Ticket') {
-        console.log('useCase:', useCase);
-        debugFunction(this.items[i]);
-      } else if (useCase === 'Legend') {
-        console.log('useCase:', useCase);
-        debugFunction(this.items[i]);
+
+        this.items[i].quality = actualQuality + 1;
+        // quality up
+        if (useCaseSellIn === 'Twice') {
+          // sellIn 10 days left: quality up
+          this.items[i].quality = actualQuality + 1;
+        } else if (useCaseSellIn === 'Third') {
+          // sellIn 5 days left: quality up
+          this.items[i].quality = actualQuality + 1;
+        }
+      } else if (useCaseName === 'Standard') {
+        // console.log('useCaseName:', useCaseName);
+        // debugFunction(this.items[i]);
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (useCaseSellIn === 'Positive' || useCaseSellIn === 'Zero') {
+          this.items[i].quality = actualQuality - 1;
+        } else {
+          this.items[i].quality = actualQuality - actualQuality;
+        }
+        // sellIn -1
+        // quality -1
       } else {
-        console.log('useCase:', useCase);
+        console.log('useCaseName:', useCaseName);
         debugFunction(this.items[i]);
+
+        // Then it is the legend object, he doesn't move
       }
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
