f715354eb2fd2624a65ae65430d6c85603b9f9d4
bsuarez- <beny.suarez_piedra@me.com>
2022-08-28 12:29:27 +0200

    [TEST]: I test all the use case
    It can be improve, but it is my first testing time

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -10,7 +10,7 @@ describe("Gilded Rose", function() {
     expect(items[0].name).to.equal("foo");
   });
 
-  it("Quality can not be negative", function() {
+  it("Quality can not be less than 0", function() {
     const gildedRose = new Shop([ new Item("Health potion", 4, 2)]);
     let items;
     for (let day = 0; day < days; day++) {
@@ -19,14 +19,93 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).to.equal(0);
   })
 
+  it("Quality can not be greather than 50", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 35, 39)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].quality).to.equal(50);
+  })
+
   it("Aged Brie quality up everyday", function() {
     const gildedRose = new Shop([ new Item("Aged Brie", 6, 2)]);
     let items;
     for (let day = 0; day < days; day++) {
       items = gildedRose.updateQuality();
-      // console.log(`${items[0].name}, ${items[0].sellIn}, ${items[0].quality}`)
     }
     expect(items[0].quality).gt(2);
+  })
+
+  it("Legendary object doesn't move", function() {
+    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].quality).to.equal(80);
+    expect(items[0].sellIn).to.equal(0);
+  })
+
+  it("Standard quality items down everyday", function() {
+    const gildedRose = new Shop([ new Item("+1 Speed Boots", 10, 40)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+      if (items[0].sellIn === 0) {
+        expect(items[0].quality).to.equal(30);
+      }
+      if (items[0].sellIn === -1) {
+        expect(items[0].quality).to.equal(28);
+      }
+    }
+    expect(items[0].quality).to.equal(20);
+  })
+
+  it("Conjured items quality down twice faster than standard items", function() {
+    const gildedRose = new Shop([ new Item("Conjured flowers", 8, 44)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+      if (items[0].sellIn === 0) {
+        expect(items[0].quality).to.equal(28);
+      }
+      if (items[0].sellIn === -1) {
+        expect(items[0].quality).to.equal(24);
+      }
+    }
+    expect(items[0].quality).to.equal(0);
+  })
+
+  it("Ticket quality increases as the concert date approaches", function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 16, 15)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+      if (items[0].sellIn === 11) {
+        expect(items[0].quality).to.equal(20);
+      }
+      if (items[0].sellIn <= 10) {
+        expect(items[0].quality).to.gte(22);
+      }
+      if (items[0].sellIn === 6) {
+        expect(items[0].quality).to.gte(30);
+      }
+      if (items[0].sellIn <= 5) {
+        expect(items[0].quality).to.gte(33);
+      }
+    }
+    expect(items[0].quality).to.equal(45);
+  })
+
+  it("Ticket quality goes to zero after the concert", function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 14, 15)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].sellIn).to.equal(-1);
+    expect(items[0].quality).to.equal(0);
   })
 
 });
