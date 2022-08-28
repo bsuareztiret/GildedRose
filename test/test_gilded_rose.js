a9bba27236e797bfa773d9219f4f8b350715cc38
bsuarez- <beny.suarez_piedra@me.com>
2022-08-28 12:03:04 +0200

    [TEST]: I try to write test
    This is the first time I try this thing, usually is better to write before coding I guess

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -2,10 +2,31 @@ var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
+  const days = 15;
+
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+    expect(items[0].name).to.equal("foo");
   });
+
+  it("Quality can not be negative", function() {
+    const gildedRose = new Shop([ new Item("Health potion", 4, 2)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].quality).to.equal(0);
+  })
+
+  it("Aged Brie quality up everyday", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 6, 2)]);
+    let items;
+    for (let day = 0; day < days; day++) {
+      items = gildedRose.updateQuality();
+      // console.log(`${items[0].name}, ${items[0].sellIn}, ${items[0].quality}`)
+    }
+    expect(items[0].quality).gt(2);
+  })
 
 });
